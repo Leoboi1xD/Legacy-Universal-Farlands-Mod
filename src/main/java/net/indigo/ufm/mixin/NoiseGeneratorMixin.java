1a4903884499e47f44c6b4cfda8bb5f55a4b512b
Leoboi1xD <leonidas.anwar@gmail.com>
2026-05-25 18:45:03 +0200

    Initial Commit

---
+++ src/main/java/net/indigo/ufm/mixin/NoiseGeneratorMixin.java
@@ -0,0 +1,47 @@
+package net.indigo.ufm.mixin;
+
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.gen.NoiseGenerator;
+import net.minecraft.world.gen.class_1779;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(NoiseGenerator.class)
+public class NoiseGeneratorMixin{
+	@Shadow private int field_111;
+	@Shadow private class_1779[] field_7557;
+
+	/**
+	 * @author Indigo227
+	 * @reason Removal of the Farlands Patch
+	 */
+	@Overwrite()
+	public double[] method_122(double[] ds, int i, int j, int k, int l, int m, int n, double d, double e, double f) {
+		if (ds == null) {
+			ds = new double[l * m * n];
+		} else {
+			for (int o = 0; o < ds.length; o++) {
+				ds[o] = 0.0;
+			}
+		}
+
+		double g = 1.0;
+
+		for (int p = 0; p < this.field_111; p++) {
+			double h = i * g * d;
+			double q = j * g * e;
+			double r = k * g * f;
+			long s = MathHelper.lfloor(h);
+			long t = MathHelper.lfloor(r);
+			h -= s;
+			r -= t;
+			h += s;
+			r += t;
+			this.field_7557[p].method_6577(ds, h, q, r, l, m, n, d * g, e * g, f * g, g);
+			g /= 2.0;
+		}
+
+		return ds;
+	}
+}
