4bce3fdb0b065de0bcc148132b5843dc76f590d1
Leoboi1xD <leonidas.anwar@gmail.com>
2026-05-28 20:04:24 +0200

    Initial Commit

--- src/main/java/net/indigo/ufm/mixin/NoiseGeneratorMixin.java
+++ src/main/java/net/indigo/ufm/mixin/NoiseGeneratorMixin.java
@@ -1,9 +1,8 @@
 package net.indigo.ufm.mixin;
 
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.noise.PerlinNoiseGenerator;
 import net.minecraft.world.gen.NoiseGenerator;
-import net.minecraft.world.gen.class_1779;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,38 +10,39 @@ import org.spongepowered.asm.mixin.Shadow;
 import java.util.Arrays;
 
 @Mixin(NoiseGenerator.class)
-public class NoiseGeneratorMixin{
-    @Shadow @Final private int field_111;
-	@Shadow @Final private class_1779[] field_7557;
-
-	/**
-	 * @author Indigo227
-	 * @reason Removal of the Farlands Patch
-	 */
-	@Overwrite()
-	public double[] method_122(double[] ds, int i, int j, int k, int l, int m, int n, double d, double e, double f) {
-		if (ds == null) {
-			ds = new double[l * m * n];
-		} else {
+public class NoiseGeneratorMixin {
+    @Shadow
+    private int field_111;
+    @Shadow
+    private PerlinNoiseGenerator[] field_110;
+    /**
+     * @author Indigo227
+     * @reason Removal of the Farlands Patch
+     */
+    @Overwrite()
+    public double[] method_122(double[] ds, int i, int j, int k, int l, int m, int n, double d, double e, double f) {
+        if (ds == null) {
+            ds = new double[l * m * n];
+        } else {
             Arrays.fill(ds, 0.0);
-		}
+        }
 
-		double g = 1.0;
+        double var27 = 1.0;
 
-		for (int p = 0; p < this.field_111; p++) {
-			double h = i * g * d;
-			double q = j * g * e;
-			double r = k * g * f;
-			long s = MathHelper.lfloor(h);
-			long t = MathHelper.lfloor(r);
-			h -= s;
-			r -= t;
-			h += s;
-			r += t;
-			this.field_7557[p].method_6577(ds, h, q, r, l, m, n, d * g, e * g, f * g, g);
-			g /= 2.0;
-		}
+        for (int var16 = 0; var16 < this.field_111; var16++) {
+            double var17 = i * var27 * d;
+            double var19 = j * var27 * e;
+            double var21 = k * var27 * f;
+            long var23 = MathHelper.lfloor(var17);
+            long var25 = MathHelper.lfloor(var21);
+            var17 -= var23;
+            var21 -= var25;
+            var17 += var23;
+            var21 += var25;
+            this.field_110[var16].method_119(ds, var17, var19, var21, l, m, n, d * var27, e * var27, f * var27, var27);
+            var27 /= 2.0;
+        }
 
-		return ds;
-	}
+        return ds;
+    }
 }
